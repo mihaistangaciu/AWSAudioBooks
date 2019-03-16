0b0f0329b46f19423538563b47937ecdc66b5bf5
Mihai Stangaciu <mstangaciu@endava.com>
2019-03-16 13:42:38 +0200

    prepare for AAD Blegrade

--- lambdas/PostReader_NewPost.py
+++ lambdas/PostReader_NewPost.py
@@ -2,15 +2,22 @@ import boto3
 import os
 import uuid
 
+
 def lambda_handler(event, context):
     
     recordId = str(uuid.uuid4())
     voice = event["voice"]
     text = event["text"]
-
+    sourceLanguage = event['sourceLang']
+    destLanguage = event['destLang']
+    
+    translate = boto3.client('translate')
+    result = translate.translate_text(Text=text, SourceLanguageCode=sourceLanguage, TargetLanguageCode=destLanguage)
+    
     print('Generating new DynamoDB record, with ID: ' + recordId)
     print('Input Text: ' + text)
     print('Selected voice: ' + voice)
+    print('Translated text:' + str(result))
     
     #Creating new record in DynamoDB table
     dynamodb = boto3.resource('dynamodb')
@@ -18,7 +25,7 @@ def lambda_handler(event, context):
     table.put_item(
         Item={
             'id' : recordId,
-            'text' : text,
+            'text' : result.get('TranslatedText'),
             'voice' : voice,
             'status' : 'PROCESSING'
         }
